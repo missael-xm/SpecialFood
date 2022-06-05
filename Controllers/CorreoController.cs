b34eeba01b7c1a947613a3504108e68405e4380c
Claudia Janeth Llamocca Saenz <claudia_llamocca@usmp.pe>
2022-06-05 01:10:50 -0500

    Inicio

---
+++ Controllers/CorreoController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SpecialFood.Models;
+using System.Net.Mail;
+
+namespace SpecialFood.Controllers
+{
+    public class CorreoController : Controller
+    {
+         private readonly ILogger<HomeController> _logger;
+
+        public CorreoController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(String Correo, String Subject, String Body, String CorreoUsu, String Nombre){
+           
+           try{
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress("milenios3dinterfaces@gmail.com"); 
+                correo.To.Add(Correo);
+                correo.Subject = Subject;
+                correo.Body = Body;
+                
+                correo.IsBodyHtml = true;
+                correo.Priority = MailPriority.Normal;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 25;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+                string sCuentaCorreo = "milenios3dinterfaces@gmail.com";
+                string sPasswordCorreo = "milenios12";
+                smtp.Credentials = new System.Net.NetworkCredential(sCuentaCorreo, sPasswordCorreo);
+
+                smtp.Send(correo);
+                ViewBag.Mensaje = "Mensaje enviado correctamente";
+           }
+           catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View();
+            
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SpecialFood.Models;
+using System.Net.Mail;
 
 namespace SpecialFood.Controllers;
 
@@ -18,10 +24,41 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+     public IActionResult Platos()
+        {
+            return View();
+        }
+
+        public IActionResult Receta()
+        {
+            return View();
+        }
+
+        public IActionResult Sangrecita()
+        {
+            return View();
+        }
+
+        public IActionResult Jugo()
+        {
+            return View();
+        }
+
+        public IActionResult Tallarines()
+        {
+            return View();
+        }
+
+        public IActionResult Torrejita()
+        {
+            return View();
+        }
+
+        public IActionResult Mazamorra()
+        {
+            return View();
+        }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
